e1716e7a3bf8e004aa2ea8b272fcd6a29d934473
namsh1125 <namsh1125@naver.com>
2022-11-04 12:05:16 +0900

    I need your help

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 
 from utils import *
+from datetime import datetime
 import time
 import cv2
 from threading import Thread
@@ -11,12 +12,24 @@ from threading import Thread
 keepRecording = True
 recorder = 0
 
+def videoRecorder():
+    height, width, _ = frame_read.frame.shape
+    # video_name = datetime.today().strftime("%Y/%m/%d %H:%M:%S")  # 촬영하기 시작한 시간으로 파일 이름 셋팅
+    # video = cv2.VideoWriter('./' + video_name + '.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
+
+    video = cv2.VideoWriter('./video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
+
+    while keepRecording:
+        video.write(frame_read.frame)
+        time.sleep(1 / 30)
+
+    video.release()
 
 if __name__ == "__main__":
 
     myDrone = initTello()
-    myDrone.takeoff()
-    time.sleep(1)
+    # myDrone.takeoff()
+    # time.sleep(1)
     myDrone.streamon()
     cv2.namedWindow("drone")
     frame_read = myDrone.get_frame_read()
@@ -24,32 +37,34 @@ if __name__ == "__main__":
 
     # Todo: keyboard to gesture
     while True:
+
+        # Todo: 카메라를 함수가 실행될 때 키는건 어떨까
         img = frame_read.frame
         cv2.imshow("drone", img)
 
         keyboard = cv2.waitKey(1)
 
-        if keyboard & 0xFF == ord('q'):
-            myDrone.land()
+        if keyboard & 0xFF == ord('q'): # 키보드에서 'q' 입력 받았을 경우
+            # myDrone.land()
             frame_read.stop()
             myDrone.streamoff()
-            keepRecording = False
-            recorder.join()
             exit(0)
             break
 
         # Todo: fix video record
-        # if keyboard & 0xFF == ord('v'):
-        #
-        #     height, width, _ = frame_read.frame_shape
-        #     video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, (width, height))  # Todo: 수업 & 수업날짜로 이름 변경
-        #
-        #     while keepRecording:
-        #         video.write(frame_read.frame)
-        #         time.sleep(1 / 30)
-        #         video.release()
+        if keyboard & 0xFF == ord('v'): # 키보드에서 'v' 입력 받았을 경우
 
-        if keyboard & 0xFF == ord('c'):  # Capture student
+            # keepRecording = True
+
+            recorder = Thread(target=videoRecorder)
+            recorder.start()
+
+            if keyboard & 0xFF == ord('v'):  # 키보드에서 'v' 입력 받았을 경우
+                keepRecording = False
+                recorder.join()
+
+        # Capture student
+        if keyboard & 0xFF == ord('c'): # 키보드에서 'c' 입력 받았을 경우
             text = 'panorama'
 
             myDrone.rotate_clockwise(30)
@@ -104,3 +119,41 @@ if __name__ == "__main__":
 
             # Todo: 이미지 이쁘게 잘라내기
             cv2.imwrite('output.jpg', dst)
+
+# import time, cv2
+# from threading import Thread
+# from djitellopy import Tello
+#
+# tello = Tello()
+#
+# tello.connect()
+#
+# keepRecording = True
+# tello.streamon()
+# frame_read = tello.get_frame_read()
+#
+# def videoRecorder():
+#     # create a VideoWrite object, recoring to ./video.avi
+#     # 创建一个VideoWrite对象，存储画面至./video.avi
+#     height, width, _ = frame_read.frame.shape
+#     video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
+#
+#     while keepRecording:
+#         video.write(frame_read.frame)
+#         time.sleep(1 / 30)
+#
+#     video.release()
+#
+# # we need to run the recorder in a seperate thread, otherwise blocking options
+# #  would prevent frames from getting added to the video
+# # 我们需要在另一个线程中记录画面视频文件，否则其他的阻塞操作会阻止画面记录
+# recorder = Thread(target=videoRecorder)
+# recorder.start()
+#
+# tello.takeoff()
+# tello.move_up(100)
+# tello.rotate_counter_clockwise(360)
+# tello.land()
+#
+# keepRecording = False
+# recorder.join()
