d4f6b5e63e16d5c1730fae7ad638ccd1d519eb68
ms987796 <ms987796@naver.com>
2022-11-10 17:52:08 +0900

    거의 완성 키키

--- main.py
+++ main.py
@@ -6,20 +6,26 @@ import time
 import os
 import sys
 import face_recognition
-#from utils import initTello
+# from utils import initTello
 from utils import *
 from datetime import datetime
 from threading import Thread
 
-
-
 myDrone = initTello()
-myDrone.takeoff()
+
+#myDrone.takeoff()
+keepRecording = True
 myDrone.streamon()
+frame_read = myDrone.get_frame_read()
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 mp_drawing_styles = mp.solutions.drawing_styles
+
+record_state = False
+
+keyboard = cv2.waitKey(1)
+
 
 def img_name(image_File):
     image_name = []
@@ -31,20 +37,26 @@ def img_name(image_File):
 def videoRecorder():
     height, width, _ = frame_read.frame.shape
 
-    today = datetime.today().strftime("%m%d-%H%M%S")  # 촬영하기 시작한 시간으로 파일 이름 셋팅
-    video_name = today + '.avi'
-    video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
+    # today = datetime.today().strftime("%m%d-%H%M%S")  # 촬영하기 시작한 시간으로 파일 이름 셋팅
+    # video_name = today + '.avi'
+    video = cv2.VideoWriter('./video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
 
     while keepRecording:
         video.write(frame_read.frame)
         time.sleep(1 / 30)
 
     video.release()
+#Ready moving
+#myDrone.move_forward(10)
+
+
 
 with mp_hands.Hands(
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as hands:
     while True:
+        if keyboard & 0xFF == ord('q'):
+            break;
         image = myDrone.get_frame_read().frame
         # Flip the image horizontally for a later selfie-view display, and convert
         # the BGR image to RGB.
@@ -59,8 +71,10 @@ with mp_hands.Hands(
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         image_height, image_width, _ = image.shape
-
-        if results.multi_hand_landmarks:
+        if not results.multi_hand_landmarks:
+            # 동작 여부 확인 후 7초뒤에 내려가기
+            print(1)
+        elif results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
 
                 # 엄지를 제외한 나머지 4개 손가락의 마디 위치 관계를 확인하여 플래그 변수를 설정합니다. 손가락을 일자로 편 상태인지 확인합니다.
@@ -115,8 +129,16 @@ with mp_hands.Hands(
                 draw = ImageDraw.Draw(image)
 
                 text = ""
-                if thumb_finger_state == 1 and index_finger_state == 1 and middle_finger_state == 1 and ring_finger_state == 1 and pinky_finger_state == 1:
-                    #text = "보"
+                if record_state:
+                    if thumb_finger_state == 1 and index_finger_state == 1 and middle_finger_state == 1 and ring_finger_state == 0 and pinky_finger_state == 0:
+                        # 녹화 종료   엄지 + 검지 + 중지
+                        time.sleep(1)
+                        keepRecording = False
+                        recorder.join()
+                        record_state = False
+                        continue
+                elif thumb_finger_state == 1 and index_finger_state == 1 and middle_finger_state == 1 and ring_finger_state == 1 and pinky_finger_state == 1:
+                    # text = "보"
                     text = 'panorama'
 
                     myDrone.rotate_clockwise(30)
@@ -172,25 +194,48 @@ with mp_hands.Hands(
                     # Todo: 이미지 이쁘게 잘라내기
                     cv2.imwrite('output.jpg', dst)
                 elif thumb_finger_state == 1 and index_finger_state == 1 and middle_finger_state == 0 and ring_finger_state == 0 and pinky_finger_state == 0:
-                    #text = "가위"
+                    # text = "가위"  동영상 녹화
+                    # 녹화 시작  엄지 + 검지
+                    record_state = True
                     recorder = Thread(target=videoRecorder)
                     recorder.start()
-
-                    # Todo: 키보드 조작으로 녹화 종료하게
-
                     myDrone.move_up(50)
-                    myDrone.rotate_counter_clockwise(90)
-                    myDrone.move_down(50)
-                    myDrone.rotate_counter_clockwise(90)
-                    myDrone.move_up(50)
-                    myDrone.rotate_counter_clockwise(90)
-                    myDrone.move_down(50)
-                    myDrone.rotate_counter_clockwise(90)
+                    myDrone.rotate_counter_clockwise(360)
+                    # myDrone.move_down(50)
+                    # myDrone.rotate_counter_clockwise(90)
+                    # myDrone.move_up(50)
+                    # myDrone.rotate_counter_clockwise(90)
+                    # myDrone.move_down(50)
+                    # myDrone.rotate_counter_clockwise(90)
 
-                    keepRecording = False
-                    recorder.join()
-                elif index_finger_state == 0 and middle_finger_state == 0 and ring_finger_state == 0 and pinky_finger_state == 0:
-                    text = "주먹"
+                if index_finger_state == 0 and middle_finger_state == 0 and ring_finger_state == 0 and pinky_finger_state == 0:
+                    # text = "주먹" 출석체크
+
+                    images = os.listdir('images')  # 폴더에 저장된 이미지 파일의 이름을 리스트로 만듦
+                    print(images)
+
+                    # myDrone.rotate_counter_clockwise(360)
+                    time.sleep(3)
+                    print("3초있다가 사진찍어욧")
+
+                    image = myDrone.get_frame_read().frame
+                    class_image = np.array(image)
+                    cv2.imwrite('class_image.jpg', class_image)
+
+                    image_to_be_matched = face_recognition.load_image_file('class_image.jpg')  # 사진에서 얼굴추출하기
+                    image_to_be_matched_encoded = face_recognition.face_encodings(image_to_be_matched)[
+                        0]  # 로드된 이미지에서 특징 추출하기   # encoded the loaded image into a feature vector
+
+                    # 모든 학생들에 대해 찍힌 사진 비교
+                    for image in images:
+                        current_image = face_recognition.load_image_file("images/" + image)  # load the image
+                        current_image_encoded = face_recognition.face_encodings(current_image)[
+                            0]  # 파일에서 가져온 이미지에서 얼굴 특징 추출하기     # encode the loaded image into a feature vector
+                        result = face_recognition.compare_faces([image_to_be_matched_encoded],
+                                                                current_image_encoded)  # 저장된 학생이 교실 사진에 있는지 없는지 확인
+
+                        if result[0] == True:  # 찍은 교실 사진에 저장된 학생이 있다면
+                            print(img_name(image), 'is here')
 
                 w, h = font.getsize(text)
 
